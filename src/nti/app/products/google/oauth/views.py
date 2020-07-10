8ec82a5933bb89d577b442d31f452be2a2d467a9
cutz <christopher.m.utz@gmail.com>
2020-07-10 11:53:13 -0500

    add views to get an authorization token outside of the login process

--- src/nti/app/products/google/oauth/views.py
+++ src/nti/app/products/google/oauth/views.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
 import pyramid.httpexceptions as hexc
+
+from pyramid.view import view_config
 
 import requests
 
@@ -14,6 +21,8 @@ from nti.app.products.google import MessageFactory as _
 
 from nti.app.products.google.oauth import OAuthError
 from nti.app.products.google.oauth import OAuthInvalidRequest
+
+from nti.dataserver.interfaces import IDataserverFolder
 
 
 import logging
@@ -102,3 +111,59 @@ def exchange_code_for_token(request, token_url=DEFAULT_TOKEN_URL):
         raise OAuthError(_('Invalid response while getting access token.'))
 
     return response.json()
+
+@view_config(name='google.oauth.authorize',
+             route_name='objects.generic.traversal',
+             context=IDataserverFolder,
+             request_method='GET',
+             renderer='rest')
+def google_oauth_authorize(request):
+    """
+    The first step in the oauth flow to obtain an authorization_token
+    from the user. The intent of this view is for use by clients with things
+    like the js FilePicker API. The client provides this view with a set of scopes. We
+    can't use https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
+    because that doesn't account for our need to bounce through the oauth portal. The process
+    is the same as documented there. Start the process and return the token via fragment on the return url.
+    """
+    params = request.params
+    scopes = [s for s in params.get('scope').split(' ') if s]
+    return initiate_oauth_flow(request,
+                               request.resource_url(request.context)+'@@google.oauth.authorize2',
+                               success=params.get('success'),
+                               failure=params.get('failure'),
+                               scopes=scopes,
+                               state=params.get('state', None))
+
+
+_FORWARDED_TOKEN_DATA = ('access_token', 'token_type', 'expires_in')
+
+def _oauth_authorize_return(url, data):
+    return '%s#%s' % (url, urlencode(data)) if data else url
+
+@view_config(name='google.oauth.authorize2',
+             route_name='objects.generic.traversal',
+             context=IDataserverFolder,
+             request_method='GET',
+             renderer='rest')
+def google_oauth2(request):
+    try:
+        data = exchange_code_for_token(request)
+
+        if 'access_token' not in data:
+            return  hexc.HTTPSeeOther(_oauth_authorize_return(request.session.get('google.failure'),
+                                                             {'error': _(u'Could not find access token.')}))
+
+        # Like https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow#oauth-2.0-endpoints_4
+        # we pass the token (or error) back in a fragment string.
+        data = {k:v for k,v in data.iteritems() if k in _FORWARDED_TOKEN_DATA}
+        response = hexc.HTTPSeeOther(_oauth_authorize_return(request.session.get('google.success'),
+                                                             data))
+    except hexc.HTTPException:
+        logger.exception('Failed to authorize with google')
+        raise
+    except Exception as e:
+        logger.exception('Failed to authorize with google')
+        response = hexc.HTTPSeeOther(_oauth_authorize_return(request.session.get('google.failure'),
+                                                             {'error': str(e)}))
+    return response
