7ee0edbdca1fe032088233a3cd625b1e5c32b373
Josh Zuech <josh.zuech@nextthought.com>
2020-07-24 12:18:52 -0500

    Add dec file

---
+++ src/nti/app/products/google/oauth/decorators.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import component
+from zope import interface
+
+from zope.container.interfaces import ILocation
+
+from nti.app.products.google.interfaces import IGoogleAPIKey
+
+from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
+
+from nti.appserver.pyramid_authorization import has_permission
+
+from nti.dataserver.authorization import ACT_READ
+
+from nti.dataserver.interfaces import IDataserverFolder
+
+from nti.externalization.interfaces import IExternalMappingDecorator
+from nti.externalization.interfaces import StandardExternalFields
+
+from nti.links.links import Link
+
+from nti.traversal.traversal import find_interface
+
+LINKS = StandardExternalFields.LINKS
+
+logger = __import__('logging').getLogger(__name__)
+
+
+def located_link(parent, link):
+    interface.alsoProvides(link, ILocation)
+    link.__name__ = ''
+    link.__parent__ = parent
+    return link
+
+
+@component.adapter(IGoogleAPIKey)
+@interface.implementer(IExternalMappingDecorator)
+class _GoogleAPIKeyDecorator(AbstractAuthenticatedRequestAwareDecorator):
+
+    def _predicate(self, context, unused_result):
+        return has_permission(ACT_READ, context, self.request)
+
+    def _do_decorate_external(self, context, result):
+        links = result.setdefault(LINKS, [])
+        link_context = find_interface(context, IDataserverFolder)
+        link = Link(link_context,
+                    elements=("@@google.oauth.authorize",),
+                    rel='google.authorize')
+        links.append(located_link(link_context, link))
